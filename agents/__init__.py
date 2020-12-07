ac38e78de64c597a1537e79442cd5ecc010a2a7a
Clay Graham <claytantor@gmail.com>
2020-12-07 13:06:10 -0800

    basic pygame

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,12 +1,12 @@
-from agents.base import *
-from agents.random import RandomAgent
-from agents.a2c import AdvantageActorCriticAgent
-from agents.ppo import ProximalPolicyOptimizationAgent
+# from agents.base import *
+# from agents.random import RandomAgent
+# from agents.a2c import AdvantageActorCriticAgent
+# from agents.ppo import ProximalPolicyOptimizationAgent
 
-A2C = AdvantageActorCriticAgent
-PPO = ProximalPolicyOptimizationAgent
+# A2C = AdvantageActorCriticAgent
+# PPO = ProximalPolicyOptimizationAgent
 
-registry = {
-    'a2c': A2C,
-    'ppo': PPO
-}
+# registry = {
+#     'a2c': A2C,
+#     'ppo': PPO
+# }
--- runner_pandas.py
+++ runner_pandas.py
@@ -11,9 +11,47 @@ import random
 
 from agents.pandas_qtable import QTableAgent
 
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+import numpy as np
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+episode_scores = []
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+
+def plot_scores(map_name=""):
+    plt.figure(1)
+    plt.clf()
+    # scores_t = torch.tensor(episode_scores, dtype=torch.float)
+    plt.title('Training {}...'.format(map_name))
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.plot(moving_average(episode_scores, 2))
+    plt.plot(moving_average(episode_scores, 100))
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        display.clear_output(wait=True)
+        display.display(plt.gcf())      
+
+
 
 def main(unused_argv):
     print("starting sc2 bot app.")
+
+    map_name="MoveToBeacon"
 
     load_dotenv(find_dotenv())
     
@@ -24,7 +62,7 @@ def main(unused_argv):
     try:
         while True:
             with sc2_env.SC2Env(
-                    map_name="MoveToBeacon",
+                    map_name=map_name,
                     players=[sc2_env.Agent(sc2_env.Race.terran, "Tergot"),
                              sc2_env.Bot(sc2_env.Race.random,
                                          sc2_env.Difficulty.very_easy)],
@@ -35,20 +73,25 @@ def main(unused_argv):
                     step_mul=16,
                     game_steps_per_episode=0,
                     realtime=False,
-                    save_replay_episodes=1,
+                    save_replay_episodes=0,
                     replay_dir=replaysDir,
-                    visualize=True) as env:
+                    visualize=False) as env:
 
                 agent.setup(env.observation_spec(), env.action_spec())
 
                 timesteps = env.reset()
                 agent.reset()
-
+                score = 0
                 while True:
                     step_actions = [agent.step(timesteps[0])]
                     if timesteps[0].last():
+                        print("score:{}".format(float(score)))
+                        episode_scores.append(float(score))
+                        plot_scores(map_name)
                         break
                     timesteps = env.step(step_actions)
+                    score += timesteps[0].reward
+                    
 
     except KeyboardInterrupt:
         pass
